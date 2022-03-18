3d5c2ba154ba15229e8c93a81ec9a6b03496fe55
Laura <laura.romano.duran@gmail.com>
2022-03-17 21:32:58 -0400

    public url images

--- src/UI/Image.js
+++ src/UI/Image.js
@@ -1,7 +1,7 @@
 import classes from "./Image.module.css";
 
 const Image = (props) => {
-  return <img src={props.url} alt="Calendar pages" className={classes.image} />;
+  return <img src={`%PUBLIC_URL%${props.url}`} alt="Calendar pages" className={classes.image} />;
 };
 
 export default Image;
