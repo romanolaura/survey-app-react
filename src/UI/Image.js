a113aa6c05e02ad78dcc832299878aadf0529f37
Laura <laura.romano.duran@gmail.com>
2022-03-18 01:31:29 -0400

    modified img link

--- src/UI/Image.js
+++ src/UI/Image.js
@@ -1,7 +1,7 @@
 import classes from "./Image.module.css";
 
 const Image = (props) => {
-  return <img src={`survey-app-react/${props.url}`} alt={props.alt || ""} className={classes.image} />;
+  return <img src={`/survey-app-react${props.url}`} alt={props.alt || ""} className={classes.image} />;
 };
 
 export default Image;
